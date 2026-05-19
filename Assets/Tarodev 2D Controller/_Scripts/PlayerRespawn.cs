192815a7221b2fc64006d879757763592aa30525
Purna <purna@users.noreply.github.com>
2026-05-19 09:07:10 +0100

    sounds

--- Assets/Tarodev 2D Controller/_Scripts/PlayerRespawn.cs
+++ Assets/Tarodev 2D Controller/_Scripts/PlayerRespawn.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
+    [SerializeField] private AudioClip deathSound;
+    [SerializeField] private float soundVolume = 1f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Hazard"))
         {
+            PlayDeathSound();
+
             GameManager.Instance.RespawnPlayer();
             Destroy(gameObject);
+        }
+    }
+
+    private void PlayDeathSound()
+    {
+        if (deathSound != null)
+        {
+            AudioSource.PlayClipAtPoint(deathSound, transform.position, soundVolume);
         }
     }
 }
